0a946759a6aa07daea5cbc8e85e58c1506164cbb
anchitdhar <anchitdhar@yahoo.com>
2020-01-24 19:22:36 +0530

    commit2

--- image_app/views.py
+++ image_app/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from django.http import HttpResponse
+from .forms import *
 # Create your views here.
+
+def hotel_image_view(request):
+    if request.method == "POST":
+        form = HotelForm(request.POST, request.FILES)
+
+            if form.is_valid():
+                form.save()
+                return redirect('Success')
+            else:
+                form = HotelForm()
+            return render(request,'index.html',{'form':form})
+
+def Success(request):
+    return HttpResponse('successfully uploaded')
